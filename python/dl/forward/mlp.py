64bce661145458b0893336588f15b6438e56087e
Yukun Song <yukunix@hotmail.com>
2017-04-26 00:20:19 +1000

    feed forward net demo

--- python/dl/forward/mlp.py
+++ python/dl/forward/mlp.py
@@ -17,13 +17,13 @@ X = dataset[:,0:8]
 Y = dataset[:,8]
 # create model
 model = Sequential()
-model.add(Dense(12, input_dim=8, init='uniform', activation='relu'))
-model.add(Dense(8, init='uniform', activation='relu'))
-model.add(Dense(1, init='uniform', activation='sigmoid'))
+model.add(Dense(12, input_dim=8, kernel_initializer='uniform', activation='relu'))
+model.add(Dense(8, kernel_initializer='uniform', activation='relu'))
+model.add(Dense(1, kernel_initializer='uniform', activation='sigmoid'))
 # Compile model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 # Fit the model
-model.fit(X, Y, nb_epoch=150, batch_size=10,  verbose=2)
+model.fit(X, Y, epochs=150, batch_size=10,  verbose=2)
 # calculate predictions
 predictions = model.predict(X)
 # round predictions
